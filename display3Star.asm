d51a3468e68932ec27b086bd5c633ca504822f54
Md Obaidur Rahman <mdobaidur0090@gmail.com>
2025-11-02 01:42:49 +0600

    first commit

---
+++ display3Star.asm
@@ -0,0 +1,21 @@
+.model small 
+.stack 100h
+.data
+    fname db "*** $"
+.code
+
+main proc
+    mov ax, @data
+    mov ds, ax
+    
+    mov ah, 9
+    lea dx, fname 
+    int 21h
+    
+    exit:
+    mov ah, 4ch
+    int 21h
+    
+    main endp
+end main
+            
